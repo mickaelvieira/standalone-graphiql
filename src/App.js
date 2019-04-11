a40984c02aae749fb9655376ba0452e662939edd
Mickael Vieira <contact@mickael-vieira.com>
2019-04-11 09:46:07 +0100

    Add GraphiQL

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GraphiQL from "graphiql";
+import fetch from "isomorphic-fetch";
+import "graphiql/graphiql.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const endpoint = `${window.location.origin}/graphql`
+
+async function graphQLFetcher(graphQLParams) {
+  const response = await fetch(endpoint, {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(graphQLParams),
+  });
+  return await response.json();
 }
+
+const App = () => (<GraphiQL fetcher={graphQLFetcher} />)
 
 export default App;
